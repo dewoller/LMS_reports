7fe792fbd7c4066834a3bf3ef8dfb5c6c2eb586b
Dennis Wollersheim <dewoller@gmail.com>
2018-08-16 16:48:07 +1000

    buildAll script

---
+++ lib/buildAll.R
@@ -0,0 +1,6 @@
+wflow_publish('analysis/processHSILogs.Rmd')
+
+wflow_publish('analysis/processIHBLogs.Rmd')
+wflow_publish('analysis/index.Rmd')
+wflow_publish('analysis/about.Rmd')
+wflow_publish('analysis/license.Rmd')
